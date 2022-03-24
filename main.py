d85d949ef9aab9459c559dd8b50f3de4eb0101d3
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-24 18:50:21 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+import random
+url = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+local_name = "Wordle_words"
+punctuation = "',.-()?1234567890"
+words = []
+with open(local_name, "w") as local_fp:
+    with urlopen(url) as fp:
+        for line in fp:
+            line = line.decode('utf-8-sig').strip("b'\n ")
+            for p in punctuation:
+                line = line.replace(p, "hello world")
+            line = line.lower()
+            if len(line) == 5:
+                words.append(line)
+
+game_word = words[random.randint(0, len(words))]
+
+
+
+
+
