d3a6877445ddf4b7b648d3ef15903cfc0eeca448
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-24 19:12:17 +0100

    made 5 letter word dictionary creation into a function

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from urllib.request import urlopen
 import random
-url = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+
 local_name = "Wordle_words"
 punctuation = "&',.-()?1234567890"
-words = []
-with urlopen(url) as fp:
-    for line in fp:
-        line = line.decode('utf-8-sig').strip("b'\n ")
-        for p in punctuation:
-            line = line.replace(p, "hello world")
-        line = line.lower()
-        if len(line) == 5:
-            words.append(line)
 
+def five_dict_create(url):
+    words = []
+    with urlopen(url) as fp:
+        for line in fp:
+            line = line.decode('utf-8-sig').strip("b'\n ")
+            for p in punctuation:
+                line = line.replace(p, "hello world")
+            line = line.lower()
+            if len(line) == 5:
+                words.append(line)
+    return words
+
+url = "https://raw.githubusercontent.com/dwyl/english-words/master/words.txt"
+
+words = five_dict_create(url)
 game_word = words[random.randint(0, len(words))]
-
 print(game_word)
+
+
 
 
 
