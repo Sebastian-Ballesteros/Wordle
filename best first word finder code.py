8b5cbbbaaf0a5099f63332febd692526f047da87
Sebastian-Ballesteros <97686942+Sebastian-Ballesteros@users.noreply.github.com>
2022-03-31 20:52:53 +0200

    wordle words

--- best first word finder code.py
+++ best first word finder code.py
@@ -1,15 +1,13 @@
-from urllib.request import urlopen
 import random
 import numpy as np
+from words import Wordle_words
+"""Word list form wordle source code"""
 
-
-
-
-def words_array(words):
-    for i in range(0, len(words)):
-        words[i] = list(words[i])
-    words_array = np.array(words)
-    return words_array
+def words_array(lst):
+    ar = lst[:]
+    for i in range(0, len(ar)):
+        ar[i] = list(ar[i])
+    return np.array(ar)
 
 
 def game(guess, game_word, words_array):
@@ -29,26 +27,36 @@ def game(guess, game_word, words_array):
     """
     Eliminating words from dictionary starts here
     """
-    pop_words = []
+    pop_words = [0]
     for word in range(0, len(words_array)):
         """If the letters are not in the guess word, discard words that contain that letter"""
         for i in letters_not_in_word:
             if i in words_array[word]:
                 pop_words.append(word)
+                break
+
+        if word == pop_words[-1]:
+            continue
 
         for letter, index in set(letters_in_word):
             """discard words that do not have a letter in guess word"""
             if letter not in words_array[word]:
                 pop_words.append(word)
+                break
 
             """discard words that have a letter in word but not in the right place, in the same place as guess word"""
             if letter == words_array[word][int(index)]:
                 pop_words.append(word)
+                break
+
+        if word == pop_words[-1]:
+            continue
 
         "discard letters that dont have the right letter in the right place"
         for letter, index in set(letters_in_word_and_place):
             if letter != words_array[word][int(index)]:
                 pop_words.append(word)
+                break
 
     pop_words = np.unique(pop_words)
     words_array = np.delete(words_array, pop_words, axis=0)
@@ -56,21 +64,24 @@ def game(guess, game_word, words_array):
     return words_array, pop_words
 
 
-words = five_dict_create(url, 5)
-words_array = words_array(words)
+words = Wordle_words[:]
+word_array = words_array(words)
+
 word_elimination_data = {}
-words = five_dict_create(url, 5)
+
+
 n = 1
 for word in words:
     avg_popped_words = []
 
     for iteration in range(0, 30):
         game_word = random.choice(words)
-        words_array_short, popped_words = game(game_word, word, words_array)
+        words_array_short, popped_words = game(game_word, word, word_array)
         avg_popped_words.append(len(list(popped_words)))
 
     word_elimination_data[str(word)] = np.mean(avg_popped_words)
     n += 100
-    print(word, len(popped_words))
-    print(n / len(words), "%")
+    print(word, word_elimination_data[word])
+    print(round(n / len(words), 1), "%")
 print(word_elimination_data)
+
